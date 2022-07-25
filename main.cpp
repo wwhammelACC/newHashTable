ba2935a2c4d7f93d6d737a178f8eb38ab26bd265
wwhammelACC <89032402+wwhammelACC@users.noreply.github.com>
2022-07-25 10:39:33 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,65 @@
+/* 
+ * your comment header here
+ */
+
+#include "main.h"
+
+int main() {
+    //seed the rand function
+    srand(time(NULL));
+
+    /*
+     * This code makes test data of 6 - 25 entries
+     * Note this guarantees at least one non unique id and one bad id
+     * Do not modify this code from here to the next comment telling
+     * you to "START HERE"
+     */
+    const int testdatasize = BASE + (rand() % OFFSET + 1);
+    int ids[testdatasize];
+    string strs[testdatasize];
+    
+    char buffer[BUFFERSIZE];
+    for (int i = 0; i < testdatasize; i++) {
+        ids[i] = rand() % MAXID + 1;
+        for (int j = 0; j < BUFFERSIZE - 1; j++) {
+            buffer[j] = 'a' + i;
+        }
+        buffer[BUFFERSIZE - 1] = '\0';
+        strs[i] = buffer;
+    }
+    ids[testdatasize-2] = ids[testdatasize-3];
+    strs[testdatasize-2] = "known duplicate";
+    ids[testdatasize-1] = - 1;
+    strs[testdatasize-1] = "known bad";
+    
+    /*
+     * Show test data
+     */
+    cout << "Showing Test Data (" << testdatasize << " entries)..." << endl;
+    for (int i = 0; i < testdatasize; i++) {
+        cout << ids[i] << " : " << strs[i] << endl;
+    }
+    cout << endl;
+
+    /*
+     * Now you have two parallel arrays with ids and strings for test data.
+     * START HERE and create your hash table and use the test data to show
+     * it works.
+     */
+    
+    // create your hash table object here
+    
+    // show it is empty by calling getCount and printTable
+    
+    // try and put ALL the test data into the table and show what happens
+
+    // continue using and testing your table, add and remove data,
+    // do whatever it takes to full test your object and prove it
+    // is robust and can handle all use cases.
+
+    
+    
+    
+    
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,22 @@
+/* 
+ * your comment header here
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>     /* cout, endl */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <string>       /* string */
+
+#define BUFFERSIZE 8
+#define MAXID 100
+#define BASE 5
+#define OFFSET 20
+
+using std::cout;
+using std::endl;
+using std::string;
+
+#endif /* MAIN_H */
