04cd926203962121635568121434e833c080cdb4
wwhammelACC <89032402+wwhammelACC@users.noreply.github.com>
2022-07-27 07:54:57 -0500

    added titles

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,3 +1,8 @@
 /* 
- * your comment header here
+Name: William Hammel
+Hash Table
+Purpose:  Create a Hash Table class/object
+with our standard struct (a struct with an integer id
+and a string for ‘data’). Implement a Separate Chaining
+(i.e. not Linear Probing) hash table class.
  */
--- main.h
+++ main.h
@@ -1,5 +1,10 @@
 /* 
- * your comment header here
+Name: William Hammel
+Hash Table
+Purpose:  Create a Hash Table class/object
+with our standard struct (a struct with an integer id
+and a string for ‘data’). Implement a Separate Chaining
+(i.e. not Linear Probing) hash table class.
  */
 
 #ifndef MAIN_H
