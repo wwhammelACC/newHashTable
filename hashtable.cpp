a8a0711c70958f5f39f45f0d281551b76af63abc
wwhammelACC <89032402+wwhammelACC@users.noreply.github.com>
2022-07-27 08:06:42 -0500

    added constructor destructor

--- hashtable.cpp
+++ hashtable.cpp
@@ -6,3 +6,28 @@ with our standard struct (a struct with an integer id
 and a string for ‘data’). Implement a Separate Chaining
 (i.e. not Linear Probing) hash table class.
  */
+
+/*
+ * ****************************************
+ * Constructors / Destructors
+ * ****************************************
+ */
+
+//constructor
+HashTable::HashTable() {
+    count = 0;
+    for(int i = 0; i < HASHTABLESIZE; i++){
+        hashtable[i] = new LinkedList;
+    }
+}
+
+//destructor
+HashTable::~HashTable() {
+    for (int i = 0; i < HASHTABLESIZE; i++) {
+        if (hashtable[i]) {
+            delete hashtable[i];
+        }
+    }
+}
+
+
